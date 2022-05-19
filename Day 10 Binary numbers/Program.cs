e379d4c97b6be6db6bcee00986c432ba72dd38a6
Safura234 <safuramg@code.edu.az>
2022-05-19 17:51:12 +0400

    https://github.com/Safura234/Binary-numbers---Hackerrank.git

---
+++ Day 10 Binary numbers/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Day_10_Binary_numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine().Trim());
+            int sum = 0;
+            int max = 0;
+            while (n > 0)
+            {
+                if (n % 2 == 1)
+                {
+                    sum++;
+
+                    if (sum > max)
+                    {
+                        max = sum;
+                    }
+                }
+                else
+                {
+                    sum = 0;
+                }
+                n = n / 2;
+            }
+            Console.WriteLine(max);
+        }
+    }
+}
